801dc875e42ea130e9226127a8a0c9235b4b146b
JulianoSilva05 <julianoqm@gmail.com>
2025-05-17 14:54:03 -0300

    Tipos de dados

---
+++ Python/01_tipos_de_dados.py
@@ -0,0 +1,7 @@
+#Por que isamos tipos?
+# Os tipos servem para definir as caracteristicas e comportamentos de um valor (objeto) para o interpretador.
+
+#Por exemplo:
+# Com esse tipo eu sou capaz de realizar operações matemáticas.
+#Esse tipo para ser armazenado em memória irá consumir 24 bytes.
+
